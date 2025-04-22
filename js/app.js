b6a558978e8151f344ec5b8e11bbec1f02a7f73b
Kauz <brubula90@gmail.com>
2025-04-21 21:47:16 -0400

    Edit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-//import { loadGLTF } from "https://cdn.jsdelivr.net/npm/mind-ar@1.1.4/dist/utils/loader.js";
-
 document.addEventListener("DOMContentLoaded", async () => {
   const mindarThree = new window.MINDAR.IMAGE.MindARThree({
     container: document.querySelector("#ar-container"),
@@ -9,7 +7,9 @@ document.addEventListener("DOMContentLoaded", async () => {
   const { renderer, scene, camera } = mindarThree;
   const anchor = mindarThree.addAnchor(0);
 
-  const model = await loadGLTF("https://www.dropbox.com/scl/fi/sh5drkjpak17r7x8jyk5w/pikachu.glb?rlkey=67wfl8kchv3mcuxmto69sb08i&st=bymih60i&raw=1");
+  // Cargar modelo GLB desde Dropbox
+  const model = await loadGLTF("https://www.dropbox.com/scl/fi/sh5drkjpak17r7x8jyk5w/pikachu.glb?rlkey=67wfl8kchv3mcuxmto69sb08i&raw=1");
+
   model.scene.scale.set(0.1, 0.1, 0.1);
   model.scene.position.set(0, 0, 0);
   anchor.group.add(model.scene);
